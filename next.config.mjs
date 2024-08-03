2ec0ef00f4c6e653d70cddbb56008d63d52b6723
Zachary Zusin <zacharyzusin@gmail.com>
2024-08-03 13:03:49 -0400

    gitignore and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+  }
+   
+  export default nextConfig;
