9785238610937aa6d4015e467cbef726ae260b97
Zachary Zusin <107821876+zacharyzusin@users.noreply.github.com>
2025-06-14 19:05:42 -0400

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -6,6 +6,13 @@ import { motion } from "framer-motion";
 
 const projectsData = [
   {
+    title: "Bird, Dog, or Reptile?",
+    description: "This project implements a hierarchical image classification model using a two-headed ResNet-50 architecture to classify animal images into both coarse (e.g., bird, dog, reptile) and fine-grained (e.g., chihuahua, rooster) categories, while handling unseen sub-classes via threshold-based novelty detection.",
+    image: "/assets/bitcoin.png",
+    githubLink: "https://github.com/zacharyzusin/Bird-Cat-Or-Dog",
+    technologies: ["PyTorch", "Python", "Machine Learning", "Computer Vision"],
+  },
+  {
     title: "Bitcoin Price Tracker",
     description: "This code implements a real-time Bitcoin price tracking application built with Elixir and Phoenix, featuring LiveView for real-time updates and PostgreSQL for price history storage.",
     image: "/assets/bitcoin.png",
